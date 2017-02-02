c0a012a3995a331289e1879e9242638b7d7d51b3
Timothy Yin <tyin92@gmail.com>
2017-02-02 17:58:58 -0500

    Added babel loader

---
+++ webPackPractice/app/index.js
@@ -0,0 +1,3 @@
+const appDiv = document.getElementById('app');
+const name = 'Kimberley';
+appDiv.innerHTML = `Welcome to ${name}'s webpack tutorial!`;
--- webPackPractice/webpack.config.js
+++ webPackPractice/webpack.config.js
@@ -1,26 +1,21 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
-
 module.exports = {
   entry: [
-    './app/index.jsx'
+    './app/index.js'
   ],
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
   module: {
     loaders: [
-      {
-        test: /\.jsx$/,
-        include: __dirname + '/app',
-        loader: 'babel?presets[]=es2015'
-      }
+      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"}
     ]
-  },
-  output: {
-    filename: 'index_bundle.js',
-    path: __dirname + '/dist'
   },
   plugins: [HTMLWebpackPluginConfig]
 };
